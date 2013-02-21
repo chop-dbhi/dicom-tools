b5887bf2d4940114efe833e30fb8625ce8df6923
Jeffrey Miller <jeffmax@gmail.com>
2013-02-21 15:40:25 -0500

    Adding simple utility to print series and study descriptions of files in a given dir.

---
+++ print_descriptions.py
@@ -0,0 +1,35 @@
+import sys
+import dicom
+import os
+
+study_descriptions = set()
+series_descriptions = set()
+print sys.argv[1]
+for root, dirs, files in os.walk(sys.argv[1]):
+     for filename in files:
+         try:
+             ds = dicom.read_file(os.path.join(root,filename))
+         except IOError:
+             sys.stderr.write("Error reading file %s\n" % os.path.join(root,filename))
+             continue
+         try:
+            study_descr = ds[0x8, 0x1030].value.strip()
+            study_descriptions.add(study_descr)
+         except KeyError:
+            pass
+
+         try:
+             series_descr = ds[0x8, 0x103E].value.strip()
+             series_descriptions.add(series_descr)
+         except KeyError:
+             pass
+
+print "--------STUDY---------"
+
+for x in study_descriptions:
+    print x
+
+print "--------SERIES---------"
+
+for x in series_descriptions:
+    print x
